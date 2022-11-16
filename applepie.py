4b78854da6910cb6775cb6b7b1f2196651d9c21b
Kai Song <95108448+KSO2525@users.noreply.github.com>
2022-11-16 18:49:32 -0500

    Add files via upload

---
+++ applepie.py
@@ -0,0 +1,44 @@
+"""
+Kai Song
+Code Clash #1
+MIT License
+
+Difficulty: 0/10
+
+My grandma and I are baking apple pies. You will be given the number of pies I am baking.
+The amount of fat (grams) I gain is x^2 the calories I eat, where x is the number of calories eaten.
+I eat 500 calories from turkey on Thanksgiving.
+Each pie is 20 calories (and I eat all the pies I make!)
+Calculate and return how much fat I gained TOTAL on Thanksgiving.
+
+Input: number of pies I will bake (int). Can be any integer from [0, 15]
+Return: TOTAL amount of fat gained (int)
+
+Test Cases:
+Input: 5    Output: 360000
+Input: 1    Output: 270400
+    
+"""
+
+class Solution:
+    def apple_pie(self, pies):
+        #TYPE pies: integer (int)
+        #RETURN: integer (int)
+        
+        #TODO: write code to calculate how much fat I gained
+        #get rid of the 'None" and return whatever you want to return
+        return None
+    
+#PLEASE DON'T TOUCH ANYTHING DOWN HERE :)))
+def main():
+    pies = int(input())
+    
+    tc1 = Solution()
+    ans = tc1.apple_pie(pies)
+    print(ans)
+
+if __name__ == "__main__":
+     main()
+     
+     
+
